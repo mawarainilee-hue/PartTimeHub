444a5607f68440800194abe026f562cc80d8124a
mawarainilee-hue <mawarainilee@gmail.com>
2026-07-27 17:11:03 +0800

    console

--- server/app.js
+++ server/app.js
@@ -27,6 +27,11 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+console.log("Cloud:", process.env.CLOUDINARY_CLOUD_NAME);
+console.log("Key:", process.env.CLOUDINARY_API_KEY);
+console.log("Secret exists:", !!process.env.CLOUDINARY_API_SECRET);
+
 // storage config
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
